cb25b4ff2ab2c6a06d5a9f153ba52e980a834a0b
AndersonPiva <andersopiva@hotmail.com>
2016-12-05 17:39:51 -0200

    aremove pagination search

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -2,7 +2,7 @@ class SearchsController < ApplicationController
   def search
     flash[:notice] = nil
     if params[:query].present?
-      @vehicles = Vehicle.where('title iLIKE ?', "%#{params[:query]}%").paginate(page: params[:page], per_page: 4)
+      @vehicles = Vehicle.where('title iLIKE ?', "%#{params[:query]}%")
       @total_vehicles = @vehicles.size
     else
       flash[:notice] = ''
